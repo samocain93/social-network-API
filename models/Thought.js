ebbe3b3b074ef546ba1be3c43c0e82a7fa1e76d1
Sam O'Cain <samocain93@gmail.com>
2023-03-08 20:38:45 -0600

    set up thought model including reactions schema

--- models/Thought.js
+++ models/Thought.js
@@ -3,16 +3,75 @@ const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 // thought schema
-const thoughtSchema = new Schema({
+const thoughtSchema = new Schema(
+  {
     thoughtText: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 280,
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
     },
     createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal),
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+
+    reactions: [reactionSchema],
+  },
+
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of reactions on retrieval
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+    });
+
+
+// reaction schema
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId();
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal),
+    },
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
     }
-})
+)
+
+// create the Thought model using the thoughtSchema
+const Thought = model('Thought', thoughtSchema);
+
+
+module.exports = Thought;
